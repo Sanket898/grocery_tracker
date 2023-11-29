810516d3d9415336529ac97dd916730ae664bc9a
SanketAM39 <Sanket@angularminds.in>
2023-11-29 17:23:30 +0530

    code optimized for tab3 page

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -8,6 +8,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class Tab3Page implements OnInit {
 
+  difference!: string | number;
+
+  formFields: Array<any> = [
+    { formControlName: 'buyPrice', label: 'Buy :', type: 'number'},
+    { formControlName: 'sellPrice', label: 'Sell :', type: 'number'},
+    { formControlName: 'quantity', label: 'Quantity :', type: 'number'},
+    { formControlName: 'stopLossPrice', label: 'Stop Loss :', type: 'number'},
+  ];
+
   constructor(private fb: FormBuilder) { }
 
   findTargetForm!: FormGroup;
@@ -22,18 +31,20 @@ export class Tab3Page implements OnInit {
       sellPrice: [''],
       stopLossPrice: [''],
       targetPrice: [''],
-      return: ['']
+      return: [''],
+      quantity: ['']
     });
   }
 
   getResult() {
     let value = (this.findTargetForm.value.buyPrice - this.findTargetForm.value.stopLossPrice) * 1.5;
-    let targetPrice = this.findTargetForm.value.stopLossPrice + value;
+    let targetPrice = +this.findTargetForm.value.stopLossPrice + value;
 
-    let buyPrice = this.findTargetForm.value.buyPrice;
-    let sellPrice = this.findTargetForm.value.sellPrice;
+    let buyPrice = this.findTargetForm.value.buyPrice * this.findTargetForm.value.quantity;
+    let sellPrice = this.findTargetForm.value.sellPrice * this.findTargetForm.value.quantity;
 
-    let result = (sellPrice * 100 / buyPrice) - 100;
+    let result = (+sellPrice * 100 / +buyPrice) - 100;
+    this.difference = sellPrice - buyPrice;
 
     this.findTargetForm.controls['targetPrice'].setValue(targetPrice);
     this.findTargetForm.controls['return'].setValue(result);
